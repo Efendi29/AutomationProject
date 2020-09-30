f08d09e43914bc8b725dc130deb036a453395271
Efendi29 <azerefem@gmail.com>
2020-09-30 22:40:42 +0200

    I have updated

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello new project for GitHub");
+        System.out.println("Hello new project for GitHub in EU3");
+    // more changes
+    // I am ready to commit
+
     }
 }
